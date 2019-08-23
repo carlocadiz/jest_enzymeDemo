e5aad27107d3f580d79ed1fd49a1bfb87778b32a
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-22 20:27:10 -0400

    test initial state and check button click

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div data-test="component-app">
-      <h1 data-test ='counter-display'>The counter is currently</h1>
-      <button data-test='increment-button'>Increment counter</button>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  render(){
+
+    return (
+      <div className='App' data-test="component-app">
+        <h1 data-test ='counter-display'>The counter is currently {this.state.counter}</h1>
+        <button
+          onClick={() => this.setState({counter: this.state.counter + 1})}
+          data-test='increment-button'
+          >
+          Increment counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
